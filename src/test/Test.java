d36f847b5c1db67439975a2c28482da1dbab99ed
jahedctg101 <49084057+jahedctg101@users.noreply.github.com>
2023-10-20 21:17:43 +0600

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
 		
 		System.out.println("New Line added 1");
-		System.out.println("====== NEW Line added 1=====");
+		
 		System.out.println("Line 3");
 		System.out.println("====== NEW Line added 1=====");
 
