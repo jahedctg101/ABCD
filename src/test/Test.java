c0815a090bbc343c9d8dbfdc30641201391872ae
jahedctg101 <49084057+jahedctg101@users.noreply.github.com>
2023-10-20 20:59:08 +0600

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,9 +3,8 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Line 1");
-		System.out.println("Line 2");
-		System.out.println("Line 3");
+
+		
 		System.out.println("New Line added 1");
 		System.out.println("====== NEW Line added 1=====");
 		System.out.println("Line 3");
