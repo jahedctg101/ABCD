1e0ae9b924e3d30520bcec18a7736ea194b66aff
jahedctg101 <49084057+jahedctg101@users.noreply.github.com>
2023-10-21 21:06:23 +0600

    commit 1

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Line 1");
-								
+		System.out.println("Line 2");						
 		System.out.println("Line 3");
 		System.out.println("====== NEW Line added 1=====");
 	}
