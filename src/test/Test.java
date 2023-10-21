cafad3f6abfe8153f284fc60a14f36279390b3b5
jahedctg101 <49084057+jahedctg101@users.noreply.github.com>
2023-10-21 08:50:07 +0600

    Commit ABCD Project

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,12 +3,10 @@ package test;
 public class Test {
 
 	public static void main(String[] args) {
-
-						
+		System.out.println("Line 1");
+		System.out.println("Line 2");						
 		System.out.println("Line 3");
 		System.out.println("====== NEW Line added 1=====");
-
-		
 	}
 
 }
